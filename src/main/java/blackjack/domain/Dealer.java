167ecd47d623cbb4d7872228b5d1fe0b1541a641
kuninaa19 <minchan0527@naver.com>
2023-01-15 00:24:41 +0900

    refactor(person): 추상클래스, 인터페이스 분리

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -1,9 +1,9 @@
 package blackjack.domain;
 
-public class Dealer extends Person {
+public class Dealer extends AbstractPerson {
 
     private static final String NAME = "딜러";
-    private static final int MONEY = 0;
+    private static final int MONEY = 1;
 
     public Dealer() {
         super(new Name(NAME), new Stake(MONEY), new Cards());
--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,6 +1,6 @@
 package blackjack.domain;
 
-public class Player extends Person {
+public class Player extends AbstractPerson {
 
     public Player(Name name, Stake stake, Cards cards) {
         super(name, stake, cards);
